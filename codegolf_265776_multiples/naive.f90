f6bbabd8aed8f6eb57f3f43caf093bf9a2af64d1
Sebastian <hbhbnr@posteo.net>
2023-10-08 01:06:58 +0200

    codegolf_265776_multiples - rename i to j and m to i

--- codegolf_265776_multiples/naive.f90
+++ codegolf_265776_multiples/naive.f90
@@ -1,13 +1,13 @@
 program x
 real::r(999)=1
 n=2
-m=100
-do i=1,999
-if(r(i)==1)then
-r(i*n)=0
-print*,i
-m=m-1
-if(m<1)stop
+i=100
+do j=1,999
+if(r(j)==1)then
+r(j*n)=0
+print*,j
+i=i-1
+if(i<1)stop
 end if
 end do
 end
