a5a95bca224fc2484e782d2e0c18f81a62f6d286
Sebastian <hbhbnr@posteo.net>
2023-10-08 00:34:24 +0200

    codegolf_265776_multiples - working, 122 bytes

--- codegolf_265776_multiples/naive.f90
+++ codegolf_265776_multiples/naive.f90
@@ -1,13 +1,13 @@
 program x
-logical::l(999)=.true.
-n=17
+logical::l(999)=1==1
+n=2
 m=100
 do i=1,999
 if(l(i))then
-l(i*n)=.false.
-print *,i
+l(i*n)=1==0
+print*,i
 m=m-1
-if (m<1) stop
+if(m<1)stop
 end if
 end do
 end
