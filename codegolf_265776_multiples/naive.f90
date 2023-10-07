b13d5b672ca31eb423854aee8867793a533b5b83
Sebastian <hbhbnr@posteo.net>
2023-10-08 00:06:17 +0200

    codegolf_265776_multiples

--- codegolf_265776_multiples/naive.f90
+++ codegolf_265776_multiples/naive.f90
@@ -1,10 +1,10 @@
 program x
-integer,parameter::n=2,m=20
+integer,parameter::n=3,m=100
 logical::l(2*m)=.true.
 do i=1,2*m
 if(l(i))then
 l(i*n)=.false.
+print *,i
 end if
 end do
-print *,findloc(l,.true.)
 end
