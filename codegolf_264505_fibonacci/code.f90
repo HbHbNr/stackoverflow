b3ab196311df16c592480bd827b62a2259cb96a0
Sebastian <hbhbnr@posteo.net>
2023-10-09 00:38:33 +0200

    codegolf_264505_fibonacci - renaming

--- codegolf_264505_fibonacci/code.f90
+++ codegolf_264505_fibonacci/code.f90
@@ -4,9 +4,9 @@ integer::i(2)=[1,0]
 character(len=99)::s
 
 !repeated code
-j=i(1)
-write(s,*)'(',j,'A)'
-if(j>0)write(*,s,advance='no')('x',k=1,j)
+l=i(1)
+write(s,*)'(',l,'A)'
+if(l>0)write(*,s,advance='no')('x',k=1,l)
 i=[i(2),sum(i)]
 
 !footer
